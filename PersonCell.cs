25778661375f202edcdf4ccaedad9b37523aeb6c
SOYHOK <100107761+SOYHOK@users.noreply.github.com>
2023-03-21 17:11:20 +0700

    Stack

---
+++ PersonCell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack1
+{
+    internal class PersonCell
+    {
+        public string Name;
+        public PersonCell Next;
+        public PersonCell() { }
+
+
+        public PersonCell(string name, PersonCell next)
+        {
+            Name = name;
+            Next = next;
+        }
+        public void push(string newName)
+        {
+            PersonCell newCell = new PersonCell();
+            newCell.Name = newName;
+            newCell.Next = this.Next;
+            this.Next = newCell;
+        }
+        public void pop(PersonCell TopCell)
+        {
+            PersonCell FirstCell = TopCell.Next;
+            TopCell.Next = FirstCell.Next;
+
+        }
+    }
+}
